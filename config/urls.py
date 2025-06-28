4381e5369b641f45492831d73be0475ac119a7c7
Faxriddin Baxtiyorov <faxriddinbaxtiyorov144@gmail.com>
2025-06-28 23:58:35 +0500

    first commit

---
+++ config/urls.py
@@ -0,0 +1,13 @@
+
+
+from django.contrib import admin
+from django.urls import path, include
+
+from .views import home
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),  # Add
+    path('',home,name='home'),
+    path('autos/', include('autos.urls')),                   # Add
+]
